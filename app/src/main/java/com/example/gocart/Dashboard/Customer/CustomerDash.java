5b7c726815bc18b3ab51d81f41c2eaf261274f6b
Krishantha Bandara <142531941+KrishanthaBSomarathna@users.noreply.github.com>
2024-08-05 14:33:00 +0530

    CustomerDash

--- app/src/main/java/com/example/gocart/Dashboard/Customer/CustomerDash.java
+++ app/src/main/java/com/example/gocart/Dashboard/Customer/CustomerDash.java
@@ -1,21 +1,37 @@
 package com.example.gocart.Dashboard.Customer;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.gocart.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CustomerDash extends AppCompatActivity {
 
     ImageView filter ;
+    private DatabaseReference mDatabase;
+    private TextView districtTextView;
+    private FirebaseAuth mAuth;
+    private TextView userTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +52,34 @@ public class CustomerDash extends AppCompatActivity {
             }
         });
 
+        districtTextView = findViewById(R.id.district);
+
+        // Initialize Firebase Database and Auth
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        String userId = mAuth.getCurrentUser().getUid();
+
+        // Load logged-in user data
+        loadCustomerDistrict(userId);
+
+    }
+    private void loadCustomerDistrict(String userId) {
+        mDatabase.child("users").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists() && snapshot.hasChild("district")) {
+                    String district = snapshot.child("district").getValue(String.class);
+                    districtTextView.setText(district);
+                    Log.d(TAG, "Customer District: " + district);
+                } else {
+                    Log.d(TAG, "Customer not found or district not available");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "loadCustomerDistrict:onCancelled", error.toException());
+            }
+        });
     }
 }
